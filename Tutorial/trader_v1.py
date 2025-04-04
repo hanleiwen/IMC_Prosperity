814f42d2fb203139197ef5392318930fbbd04d1e
hanleiwen <wenhanlei2021@outlook.com>
2025-04-03 23:42:36 -0500

    BAD

--- Tutorial/trader_v1.py
+++ Tutorial/trader_v1.py
@@ -1,17 +1,38 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import math
+import numpy as np
 import jsonpickle as jp
 
 class Trader:
-    WINDOW_SIZE = 10  # For moving average
+    WINDOW_SIZE = 6  # For moving average
 
     def __init__(self):
-        self.trader_data = []
+        self.price_history = []
+        self.volume_history = []
 
-    def fair(self, order_depth: OrderDepth, method = "mid_price", vol_filter = 0):
-        theo = None
-        if order_depth.sell_orders and order_depth.buy_orders:
+    def fair(self, product: str, order_depth: OrderDepth, method = "mid_price", vol_filter = 0):
+        
+        fair_price = None
+
+        if method == "mid_price":
+            best_ask = min(order_depth.sell_orders.keys())
+            best_bid = max(order_depth.buy_orders.keys())
+
+            fair_price = (best_ask + best_bid) / 2
+        
+        elif method == "mid_price_with_vol_filter":
+            if len([price for price in order_depth.sell_orders.keys() if abs(order_depth.sell_orders[price]) >= vol_filter]) == 0 or \
+                len([price for price in order_depth.buy_orders.keys() if abs(order_depth.buy_orders[price]) >= vol_filter]) == 0:
+                best_ask = min(order_depth.sell_orders.keys())
+                best_bid = max(order_depth.buy_orders.keys())
+            else:
+                best_ask = min([price for price in order_depth.sell_orders.keys() if abs(order_depth.sell_orders[price]) >= vol_filter])
+                best_bid = max([price for price in order_depth.buy_orders.keys() if abs(order_depth.buy_orders[price]) >= vol_filter])
+
+            fair_price = (best_ask + best_bid) / 2
+
+        elif method == "moving_average":
             best_bid = max(order_depth.buy_orders.keys())
             best_ask = min(order_depth.sell_orders.keys())
             mid_price = (best_bid + best_ask) / 2
@@ -23,33 +44,13 @@ class Trader:
             
             # Update price and volume history
             self.price_history.append(mid_price)
-            self.volume_history.append(total_vol)
             
             # Simple moving average
             if len(self.price_history) >= self.WINDOW_SIZE:
-                theo = np.mean(self.price_history[-self.WINDOW_SIZE:])
+                fair_price = np.mean(self.price_history[-self.WINDOW_SIZE:])
             else:
-                theo = mid_price
-        return theo
-        # best_ask = 0
-        # best_bid = 0
-        # mid_price = 0
-
-        # if method == "mid_price":
-        #     best_ask = min(order_depth.sell_orders.keys())
-        #     best_bid = max(order_depth.buy_orders.keys())
-            
-        # elif method == "mid_price_with_vol_filter":
-        #     if len(price for price in order_depth.sell_orders.keys() if abs(order_depth.sell_orders[price]) >= vol_filter) == 0 or \
-        #         len(price for price in order_depth.buy_orders.keys() if abs(order_depth.buy_orders[price]) >= vol_filter) == 0:
-        #         best_ask = min(order_depth.sell_orders.keys())
-        #         best_bid = max(order_depth.buy_orders.keys())
-        #     else:
-        #         best_ask = min([price for price in order_depth.sell_orders.keys() if abs(order_depth.sell_orders[price]) >= vol_filter])
-        #         best_bid = max([price for price in order_depth.buy_orders.keys() if abs(order_depth.buy_orders[price]) >= vol_filter])
-        
-        # mid_price = (best_ask + best_bid) / 2
-        # return mid_price 
+                fair_price = mid_price
+        return fair_price
     
     def clear_position_order(self, orders: List[Order], order_depth: OrderDepth, position: int, position_limit: int, 
                              product: str, buy_order_volume: int, sell_order_volume: int, fair_value: float) -> List[Order]:
@@ -83,8 +84,8 @@ class Trader:
         buy_order_volume = 0
         sell_order_volume = 0
 
-        ''' ba = fair_value + width
-        bb = fair_value - width ''' 
+        # ba = fair_value + width
+        # bb = fair_value - width 
 
         if len(order_depth.sell_orders) != 0:
             best_ask = min(order_depth.sell_orders.keys())
@@ -106,15 +107,15 @@ class Trader:
         
         buy_order_volume, sell_order_volume = self.clear_position_order(orders, order_depth, position, position_limit, product, buy_order_volume, sell_order_volume, fair_value)
 
-        ''' 
-        buy_quantity = position_limit - (position + buy_order_volume)
-        if buy_quantity > 0:
-            orders.append(Order(product, ba + width, buy_quantity))  # Buy order
+        
+        # buy_quantity = position_limit - (position + buy_order_volume)
+        # if buy_quantity > 0:
+        #     orders.append(Order(product, ba + width, buy_quantity))  # Buy order
 
-        sell_quantity = position_limit + (position - sell_order_volume)
-        if sell_quantity > 0:
-            orders.append(Order("AMETHYSTS", bb - width, -sell_quantity))  # Sell order 
-        '''
+        # sell_quantity = position_limit + (position - sell_order_volume)
+        # if sell_quantity > 0:
+        #     orders.append(Order(product, bb - width, -sell_quantity))  # Sell order 
+        
 
         return orders
 
@@ -123,12 +124,12 @@ class Trader:
 
         # params for rainforest resin
         rr_position_limit = 50
-        rr_fair_value = 10000
+        rr_fair_value = self.fair("RAINFOREST_RESIN", state.order_depths["RAINFOREST_RESIN"])
         rr_width = 1
 
         # params for kelp
         kelp_position_limit = 50
-        kelp_fair_value = 2025
+        kelp_fair_value = self.fair("KELP", state.order_depths["KELP"], method = "moving_average")
         kelp_width = 1
         
         if "RAINFOREST_RESIN" in state.order_depths:
